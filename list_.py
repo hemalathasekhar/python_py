165d4fd12be971774e19722908c1f92573ce9dec
hemalathasekhar <113087461+hemalathasekhar@users.noreply.github.com>
2022-09-08 16:11:33 +0530

    Add files via upload

---
+++ list_.py
@@ -0,0 +1,53 @@
+ # list
+# data=[1,2,3,4,5]
+#data=["a","b","c","d"]
+# data.append("e")
+# data.clear()
+# print(data)
+# print(type(data))
+# print(dir(data))
+#a=(1,2,3,4)
+#data.insert(2,a)
+#print(data)
+# data.clear()
+# data2=data.copy()
+# print(data)
+# print(data2)
+# print(type(data))
+# print(dir(data))
+# print(data.count("c"))
+# print(data.count("b"))
+# print(data.index("a"))
+
+'''
+pop
+remove
+reverse
+sort
+'''
+#pop
+batch=[22,23,32,34,]
+print(batch)
+print(batch.pop(1))
+
+#remove
+#s=["a","b","c","d","e","f"]
+v=[1,2,3,4,5]
+print(v)
+print(v.remove(3))
+#print(s.remove("c"))
+
+#reverse
+ 
+w=[29,67,49,13]
+Class=["D","R","S","E","G","J"]
+Class.sort(reverse=True)
+w.sort(reverse=True)
+w.sort(reverse=False)
+Class.reverse()
+print(Class)
+
+#sort
+name=["a","c","e","b","d","f"]
+name.sort()
+print(name)
